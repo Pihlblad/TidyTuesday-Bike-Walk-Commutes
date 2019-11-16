065c78d38e86048b82e2ff03e52c67c64dd245d8
Johanna Pihlblad <johanna.pihlblad@westernsydney.edu.au>
2019-11-16 13:19:23 +1100

    load pkgs

--- run.R
+++ run.R
@@ -1,4 +1,12 @@
 # TidyTuesday "Modes Less Traveled - Bicycling and Walking to Work in the United States: 2008-2012"
 # R_meetup 7th November 2019
 # GET THE DATA
-commute_mode <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-11-05/commute.csv")
+commute_mode <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-11-05/commute.csv") 
+
+# load pakages
+library(tidyverse)
+library(sf) #GIS package
+library(ggrepel)# makes lables and points not overlap
+library(ggspatial) # allowes usage of GIS layers from sf pkg in ggplot
+library(maps) # spatial data 
+library(wesanderson) #color palette
