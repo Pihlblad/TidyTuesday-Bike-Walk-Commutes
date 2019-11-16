668e5fbac8b13cff12a743ce0ab1399bc17d8e8e
Johanna Pihlblad <johanna.pihlblad@westernsydney.edu.au>
2019-11-16 13:17:31 +1100

    start project and load TT data

---
+++ run.R
@@ -0,0 +1,4 @@
+# TidyTuesday "Modes Less Traveled - Bicycling and Walking to Work in the United States: 2008-2012"
+# R_meetup 7th November 2019
+# GET THE DATA
+commute_mode <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-11-05/commute.csv")
