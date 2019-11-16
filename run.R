d4d183a520e3f45ff94bdffaac2824b2d2f0a04e
Johanna Pihlblad <johanna.pihlblad@westernsydney.edu.au>
2019-11-16 13:29:00 +1100

    ggplot segment

--- run.R
+++ run.R
@@ -13,6 +13,7 @@ library(wesanderson) #color palette
 
 #spatial data available from the "maps" package
 data(us.cities)
+
 # Commuter data prep
 cities <- commute_mode %>% 
   separate(city, 
@@ -24,3 +25,23 @@ cities <- commute_mode %>%
   ) %>% 
   left_join(us.cities, by="name") %>% 
   filter(capital ==2) # 2 = state capital
+
+# mapping cummuter stats in US capital cities with ggplot
+ggplot(cities, aes(long, lat))+
+  borders("state", fill = "antiquewhite", colour = gray(0.4))+ #border polygon from the maps pkg
+  geom_point(colour="black") +
+  geom_label_repel(aes(fill=percent, label = name,label.size = 0.1))+
+  scale_fill_gradientn(colours = wes_palette(n=20, name="GrandBudapest2",
+                                             type="continuous"))+
+  coord_sf(xlim = c(-125.15, -69.12), ylim = c(24.65, 50.97))+ #Lat/Long extent to be shown
+  facet_grid(mode~.)+ 
+  theme_bw()+ 
+  theme(panel.grid.major = element_line(color = gray(.75), 
+                                        linetype ="dashed", 
+                                        size = 0.5), 
+        panel.background = element_rect(fill = "aliceblue"))+
+  labs(title="Modes Less Traveled", 
+       subtitle = "% Biking and Walking to Work in State Capitals: 2008-2012",
+       caption = "source: U.S. Cencus Bureau, American Community Survey, 2008-2012 & #TidyTuesday ")+
+  xlab("Longitude") + 
+  ylab("Latitude")
